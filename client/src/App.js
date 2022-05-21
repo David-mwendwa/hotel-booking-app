4c2c8d0f48128fdf87d7a57ed8023251656ed77a
techdave <klatzdavie19@gmail.com>
2022-05-21 15:18:31 +0300

    implement routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './booking/Home';
+import Register from './auth/Register';
+import Login from './auth/Login';
+
 function App() {
-  return <div>Hotel Booking App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
