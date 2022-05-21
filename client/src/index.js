2736475b62776b4b2b179d7f7c34c03cb38dcaed
techdave <klatzdavie19@gmail.com>
2022-05-21 16:31:13 +0300

    refactor redux logic to redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,29 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-// create user reducer function
-const authReducer = (state = {}, action) => {
-  switch (action.type) {
-    case 'LOGGED_IN_USER':
-      return {
-        ...state,
-        ...action.payload,
-      };
-    case 'LOGOUT':
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-// combine multiple reducers
-const rootReducer = combineReducers({
-  user: authReducer,
-});
+import rootReducer from './reducers';
 
 // create redux store
 const store = createStore(rootReducer, composeWithDevTools());
