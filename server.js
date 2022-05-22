f6a5b16252785f999272e8d031ac22a9e5f062ce
techdave <klatzdavie19@gmail.com>
2022-05-22 08:09:50 +0300

    create basic express server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('=============> NODE SERVER!');
+import express from 'express';
+const app = express();
+
+app.get('/api/:message', (req, res) => {
+  res.status(200).json({msg: req.params.message});
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server running on port ${port}...`));
