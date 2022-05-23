3091e8670b43b1ddbb3b1b64839cc48d2e1637fa
techdave <klatzdavie19@gmail.com>
2022-05-23 13:34:08 +0300

    add pre save hook for hashing password in the userSchema - bcypt

--- models/user.js
+++ models/user.js
@@ -1,24 +1,42 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, 'Please provide your name'],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide your name'],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Please provide your email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      min: 6,
+    },
+    stripe_account_id: '',
+    stripe_seller: {},
+    stripeSession: {},
   },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, 'Please provide your email'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide password'],
-    min: 6,
-  },
-  stripe_account_id: '',
-  stripe_seller: {},
-  stripeSession: {},
+  { timestamps: true }
+);
+
+userSchema.pre('save', function (next) {
+  let user = this;
+  if (user.isModified('password')) {
+    return bcrypt.hash(user.password, 12, function (err, hash) {
+      if (err) {
+        console.log('BCRYPT HASH ERR ', err);
+        return next(err);
+      }
+      user.password = hash;
+      return next();
+    });
+  } else return next();
 });
 
 export default mongoose.model('User', userSchema);
