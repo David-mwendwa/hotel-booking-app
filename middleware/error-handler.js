511407443f493c401246cabdbc31f4e7caa80a2e
techdave <klatzdavie19@gmail.com>
2022-05-23 14:04:23 +0300

    add error middlewares

---
+++ middleware/error-handler.js
@@ -0,0 +1,55 @@
+import { StatusCodes } from 'http-status-codes';
+
+const errorHandlerMiddleware = async (err, req, res, next) => {
+  const defaultError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, please try again later',
+  };
+  // handle wrong mongoose object ID error
+  if (err.name === 'CastError') {
+    defaultError.statusCode = StatusCodes.NOT_FOUND;
+    defaultError.msg = `Resource not found. Invalid: ${err.path}`;
+  }
+
+  // handle missing field error
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    let message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+    let fields = [];
+    let regex = /`(\w+)`/g;
+    let match = regex.exec(message);
+    while (match !== null) {
+      fields.push(match[1]);
+      match = regex.exec(message);
+    }
+    defaultError.msg =
+      (fields.length &&
+        `${fields.join(', ')} ${
+          fields.length > 1 ? 'fields are' : 'field is'
+        } required`) ||
+      message;
+  }
+
+  // handle unique value/duplicate key error
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field must be unique`;
+  }
+
+  // handle wrong jwt error
+  if (err.name === 'JsonWebTokenError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `JSON Web Token is invalid. Please try again.`;
+  }
+
+  // handle expired jwt error
+  if (err.name === 'TokenExpiredError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `JSON Web Token is expired. Please try again.`;
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+};
+
+export default errorHandlerMiddleware;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import 'express-async-errors';
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
@@ -9,12 +10,20 @@ dotenv.config();
 // import routers
 import authRouter from './routes/auth.js';
 
+// import custom middlewares
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
+
 // middlewares
 app.use(cors()); // NOT REALLY USABLE
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/v1/auth', authRouter);
+
+// use error middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 // start server
 const port = process.env.PORT || 5000;
@@ -23,7 +32,7 @@ const start = async () => {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () => console.log(`server running on port ${port}...`));
   } catch (error) {
-     console.log(error);
+    console.log(error);
   }
-}
-start()
+};
+start();
