53f42f5036a06bc8e8f05e02be21e747a3c25e3b
techdave <klatzdavie19@gmail.com>
2022-05-23 15:17:19 +0300

    use react-toastify to show success and error message to client

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,24 @@ import Home from './booking/Home';
 import Register from './auth/Register';
 import Login from './auth/Login';
 import TopNav from './components/TopNav';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
       <TopNav />
+      <ToastContainer
+        position='top-right'
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/register' element={<Register />} />
--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import RegisterForm from '../components/RegisterForm';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Register = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -12,9 +15,10 @@ const Register = () => {
     try {
       const input = { name, email, password };
       const { data } = await axios.post('/api/v1/auth/register', input);
-      console.log({ data });
+      if (data.ok) toast.success('Registered successfully');
+      setTimeout(() => navigate('/'), 3000);
     } catch (error) {
-      console.log(error.response.data.msg);
+      toast.error(error.response.data.msg);
     }
   };
 
@@ -23,6 +27,7 @@ const Register = () => {
       <div className='container-fluid bg-secondary p-5 text-center'>
         <h1>Register</h1>
       </div>
+
       <div className='container'>
         <div className='row'>
           <div className='col-md-6 offset-md-3'>
