0caebd549e25299688a955226c8e046c3139b42b
techdave <klatzdavie19@gmail.com>
2022-05-27 19:16:36 +0300

    refactor auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,35 +2,6 @@ import jwt from 'jsonwebtoken';
 import { UnauthenticatedError, ForbiddenError } from '../errors/index.js';
 import { isTokenValid } from '../utils/auth.js';
 
-// checks if user is authenticated or not - through bearer token
-export const authenticateUser_bearer = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnauthenticatedError('Authentication Invalid');
-  }
-  const token = authHeader.split(' ')[1];
-  try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { userId: payload.userId, role: payload.role };
-    next();
-  } catch (error) {
-    throw new UnauthenticatedError('Authentication Invalid');
-  }
-};
-
-// checks if user is authenticated or not - through cookies
-export const authenticateUser_cookies = async (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token) {
-    throw new UnauthenticatedError(
-      'You must login first to access this resource'
-    );
-  }
-  const { userId, role } = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = { userId: userId, role: role };
-  next();
-};
-
 // checks user authentication through bearer token or through cookies combo
 export const authenticateUser = async (req, res, next) => {
   let token;
