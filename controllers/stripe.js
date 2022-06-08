3b7edc2341c3843fbaaa4e58b094d69556568b35
techdave <klatzdavie19@gmail.com>
2022-06-08 19:58:13 +0300

    start on createConnectAccount controller

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,7 +1,20 @@
+import Stripe from 'stripe';
 import User from '../models/user.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError } from '../errors/index.js';
 
+const stripe = Stripe(process.env.STRIPE_SECRET);
+console.log(Object.keys(stripe.accounts._stripe));
+
 export const createConnectAccount = async (req, res) => {
+  const user = await User.findById(req.user.userId);
+  if (!user.stripe_account_id) {
+    const account = await stripe.accounts.create({
+      type: 'express',
+    });
+    console.log({ account });
+  }
+  user.stripe_account_id = account.id;
+  user.save();
   res.status(StatusCodes.CREATED).json({ ok: true });
 };
