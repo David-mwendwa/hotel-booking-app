88cd0be5e8b3633d24340260e27065644d464de4
techdave <klatzdavie19@gmail.com>
2022-06-13 09:29:21 +0300

    get account status on stripe callback

--- client/src/stripe/StripeCallback.js
+++ client/src/stripe/StripeCallback.js
@@ -1,8 +1,24 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoadingOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAccountStatus } from '../redux/actions/stripe';
+import { toast } from 'react-toastify';
 
 const StripeCallback = () => {
+  const { user, token, error } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getAccountStatus(token));
+    }
+
+    if (error) {
+      toast.error(error);
+    }
+  }, [dispatch, error, token, user]);
+
   return (
     <div className='d-flex justify-content-center p-5'>
       <LoadingOutlined className='display-1 h1 p-5 text-danger' />
