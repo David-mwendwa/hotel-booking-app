f81fae685e51f9207f529929daeea025977a6ac6
techdave <klatzdavie19@gmail.com>
2022-06-18 20:11:27 +0300

    check token

--- client/src/stripe/StripeCallback.js
+++ client/src/stripe/StripeCallback.js
@@ -12,6 +12,7 @@ const StripeCallback = () => {
 
   useEffect(() => {
     if (token) {
+      console.log(token)
       dispatch(getAccountStatus(token));
       dispatch(updateUserInLocalStorage({ user, token }));
     }
